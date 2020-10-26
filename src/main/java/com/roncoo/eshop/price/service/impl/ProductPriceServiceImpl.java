79dd80d583fd4f5bc6dd48f91b8f6a429a097b6a
kidd <2737047953@qq.com>
2020-10-26 23:50:00 +0800

    新增one service

--- src/main/java/com/roncoo/eshop/price/service/impl/ProductPriceServiceImpl.java
+++ src/main/java/com/roncoo/eshop/price/service/impl/ProductPriceServiceImpl.java
@@ -42,5 +42,16 @@ public class ProductPriceServiceImpl implements ProductPriceService {
 	public ProductPrice findById(Long id) {
 		return productPriceMapper.findById(id);
 	}
-
+	
+	public ProductPrice findByProductId(Long productId) {
+		Jedis jedis = jedisPool.getResource();
+		String dataJSON = jedis.get("product_price_" + productId);
+		if(dataJSON != null && !"".equals(dataJSON)) {
+			JSONObject dataJSONObject = JSONObject.parseObject(dataJSON);
+			dataJSONObject.put("id", "-1");  
+			return JSONObject.parseObject(dataJSONObject.toJSONString(), ProductPrice.class);
+		} else {
+			return productPriceMapper.findByProductId(productId);
+		}
+	}
 }
